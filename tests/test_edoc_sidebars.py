50bf8da513d1b2ee72d3071e67866b40c260e85c
SergPudikov <serg.pudikov@admortgage.com>
2022-09-08 18:39:31 +0300

    fixed tests for new edoc version

--- tests/test_edoc_sidebars.py
+++ tests/test_edoc_sidebars.py
@@ -9,29 +9,13 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-# Loans to use: 1006510, 1003636
+# Loans to use: 1006510, 1003636, 1006508
 
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.get('https://edoc.admortgage.us/loan/1006510')  # Any URL applicable
-    input_username = wait_of_element_located(xpath='//*[@id=":r0:"]', driver=driver)
-    input_password = wait_of_element_located(xpath='//*[@id=":r1:"]', driver=driver)
-    login_button = wait_of_element_located(xpath='//*[@id=":r2:"]', driver=driver)
-
-    input_username.send_keys('testUser')  # Any username applicable
-    input_password.send_keys('Pass')  # Any password applicable
-    login_button.click()
-    yield driver
-    driver.close()
-
-
-@pytest.fixture
-def driver2():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get('https://edoc.admortgage.us/login')  # Any URL applicable
+    driver.get('https://edoc.admortgage.us/loan/1006508')  # Any URL applicable
     input_username = wait_of_element_located(xpath='//*[@id=":r0:"]', driver=driver)
     input_password = wait_of_element_located(xpath='//*[@id=":r1:"]', driver=driver)
     login_button = wait_of_element_located(xpath='//*[@id=":r2:"]', driver=driver)
@@ -64,7 +48,8 @@ def wait_id(id, driver):
 def test_searchbar_positive(driver):
     searchbar = wait_of_element_located(xpath='//*[@id="input-with-icon-textfield"]', driver=driver)
     searchbar.click()
-    searchbar.send_keys('closing')  # Any CORRECT value applicable
+    searchbar.send_keys('tran')  # Any CORRECT value applicable
+    time.sleep(5)
     result = wait_of_element_located(
         xpath='/html/body/div[1]/main/div[2]/div[1]/div/div[3]/ul/div/div[2]/div/div/div/div/div', driver=driver)
     result.click()
@@ -91,30 +76,28 @@ def test_download(driver):
 
 
 def test_feed(driver):
-    feed = wait_of_element_located(xpath='//*[@id="root"]/main/div[2]/div[3]/div/div/div[1]/div', driver=driver)
-    feed.click()
-
     try:
-        wait_of_element_located(xpath='//*[@id="root"]/main/div[2]/div[3]/div/div/div[2]/div/div/div/div/ul',
-                                driver=driver)
+        wait_of_element_located(xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[2]', driver=driver)
     except NoSuchElementException:
         assert False
     assert True
 
 
-def test_main_properties(driver):
+def test_properties(driver):
     searchbar = wait_of_element_located(xpath='//*[@id="input-with-icon-textfield"]', driver=driver)
     searchbar.click()
-    searchbar.send_keys('closing')  # Any CORRECT value applicable
+    searchbar.send_keys('tran')  # Any CORRECT value applicable
     result = wait_of_element_located(
         xpath='/html/body/div[1]/main/div[2]/div[1]/div/div[3]/ul/div/div[2]/div/div/div/div/div', driver=driver)
     result.click()
-    main_prop = wait_of_element_located(xpath='//*[@id="root"]/main/div[2]/div[3]/div/div[2]/div[1]/div', driver=driver)
-    main_prop.click()
+    properties = wait_of_element_located(
+        xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[1]/div/div/div/button[2]', driver=driver)
+    properties.click()
 
     try:
-        wait_of_element_located(xpath='//*[@id="root"]/main/div[2]/div[3]/div/div[2]/div[2]/div/div/div/div',
-                                driver=driver)
+        wait_of_element_located(
+            xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[3]/div[1]/div[2]/div/div/div/div',
+            driver=driver)
     except NoSuchElementException:
         assert False
     assert True
@@ -123,47 +106,44 @@ def test_main_properties(driver):
 def test_version_history(driver):
     searchbar = wait_of_element_located(xpath='//*[@id="input-with-icon-textfield"]', driver=driver)
     searchbar.click()
-    searchbar.send_keys('closing')  # Any CORRECT value applicable
+    searchbar.send_keys('tran')  # Any CORRECT value applicable
     result = wait_of_element_located(
         xpath='/html/body/div[1]/main/div[2]/div[1]/div/div[3]/ul/div/div[2]/div/div/div/div/div', driver=driver)
     result.click()
-    vers_hist = wait_of_element_located(xpath='//*[@id="root"]/main/div[2]/div[3]/div/div[3]/div[1]/div', driver=driver)
+    vers_hist = wait_of_element_located(
+        xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[1]/div/div/div/button[3]', driver=driver)
     vers_hist.click()
 
     try:
         wait_of_element_located(
-            xpath='//*[@id="root"]/main/div[2]/div[3]/div/div[3]/div[2]/div/div/div/div/div/div/div/div[1]',
+            xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[4]',
             driver=driver)
     except NoSuchElementException:
         assert False
     assert True
 
 
-def test_change_description(driver2):
-    wait_id(id=':r3:', driver=driver2).send_keys('1006508')
-    wait_of_element_located(xpath='/html/body/div[3]/div[3]/div/a/div/p[1]', driver=driver2).click()
-    wait_of_element_located(xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[1]/div/div[1]/div/span',
-                            driver=driver2).click()
-    a = wait_of_element_located(
-        xpath='//*[@id="root"]/main/div[2]/div[3]/div/div/div[2]/div/div/div/div/ul/div[1]/li/div[2]/p/a',
-        driver=driver2)
-    action = ActionChains(driver2)
-    action.click_and_hold(a)
-    action.release(a)
-    action.perform()
-    wait_of_element_located(xpath='/html/body/div[1]/main/div[2]/div[3]/div/div[1]/div[1]/div', driver=driver2).click()
-    wait_of_element_located(xpath='/html/body/div[1]/main/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div/span',
-                            driver=driver2).click()
-    b = wait_id(id=':re:', driver=driver2)
-    action.click(b)
+def test_change_description(driver):
+    wait_of_element_located(xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[2]/div/ul/div[1]/li/div[2]/p/a',
+                            driver=driver).click()
+    wait_of_element_located(xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[1]/div/div/div/button[2]',
+                            driver=driver).click()
+    wait_of_element_located(xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[3]/div[1]/div[1]/div/div[1]',
+                            driver=driver).click()
+    desc = wait_of_element_located(
+        xpath='/html/body/div[1]/main/div[2]/div[3]/div/div/div[3]/div[1]/div[2]/div/div/div/div/div[6]/div/div/textarea[1]',
+        driver=driver)
+    time.sleep(5)
+    action = ActionChains(driver)
+    action.click(desc)
     action.key_down(Keys.CONTROL)
     action.send_keys('A')
     action.key_up(Keys.CONTROL)
     action.send_keys('aaa111')
     action.perform()
-    wait_of_element_located(xpath='//*[@id="root"]/header/div/div[1]/div', driver=driver2).click()
-    time.sleep(10)
-    c = wait_of_element_located(
-        xpath='//*[@id=":r6:"]/div[2]/div[2]/div/div/div/div/div[1]/div/div/div[1]/div/div[2]/span',
-        driver=driver2)
-    assert c.text == 'aaa111'
+    wait_of_element_located(xpath='//*[@id="root"]/header/div/div[1]/div', driver=driver).click()
+    time.sleep(5)
+    desc2 = wait_of_element_located(
+        xpath='/html/body/div[1]/main/div[2]/div[1]/div/div[3]/ul/div[2]/div[2]/div/div/div/div/div[1]/div/div/div[1]/div/div[2]/span',
+        driver=driver)
+    assert desc2.text == 'aaa111'
